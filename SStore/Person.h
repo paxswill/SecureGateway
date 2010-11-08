357aeffda6375c90becaa5833908c7076e59c89c
Will Ross <paxswill@gmail.com>
2010-11-08 11:12:50 -0500

    Person class header created

---
+++ SStore/Person.h
@@ -0,0 +1,34 @@
+#ifndef PERSON_CC_9QDZN2ZE
+#define PERSON_CC_9QDZN2ZE
+
+#include <string>
+
+class Person
+{
+public:
+	Person (std::string initialName);
+	virtual ~Person ();
+	
+	//Getters
+	int getID();
+	std::string getName();
+	bool isAdmin();
+	long getPWHash();
+	std::string getEmail();
+	
+	//Setters
+	void setName(std::string password, std::string newName);
+	void setAdmin(Person settingAdmin, bool adminStatus);
+	void resetPassword();
+	void setPassword(std::string oldPassword, std::string newPassword);
+	void setEmail(std::string password, std::string newEmail);
+	
+private:
+	int id;
+	std::string name;
+	bool admin;
+	long passwordHash;
+	std::string email;
+};
+
+#endif /* end of include guard: PERSON_CC_9QDZN2ZE */
