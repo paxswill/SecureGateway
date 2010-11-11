88b9dc1f5b61c2553672c2168b93cff4f2895951
Will Ross <paxswill@gmail.com>
2010-11-10 23:02:22 -0500

    Initial Paring of Model Object methods
    When removed, the methods are replaced with optimized versions.

--- SStore/Source/Model Objects/PXStudent.m
+++ SStore/Source/Model Objects/PXStudent.m
@@ -12,36 +12,4 @@
 @implementation PXStudent
 @dynamic enrolledCourses;
 
-#if 0
-/*
- *
- * Property methods not providing customized implementations should be removed.  
- * Optimized versions will be provided dynamically by the framework at runtime.
- *
- *
-*/
-
-- (PXCourse *)enrolledCourses {
-    id tmpObject;
-    
-    [self willAccessValueForKey:@"enrolledCourses"];
-    tmpObject = [self primitiveEnrolledCourses];
-    [self didAccessValueForKey:@"enrolledCourses"];
-    
-    return tmpObject;
-}
-
-- (void)setEnrolledCourses:(PXCourse *)value {
-    [self willChangeValueForKey:@"enrolledCourses"];
-    [self setPrimitiveEnrolledCourses:value];
-    [self didChangeValueForKey:@"enrolledCourses"];
-}
-
-- (BOOL)validateEnrolledCourses:(id *)valueRef error:(NSError **)outError {
-    // Insert custom validation logic here.
-    return YES;
-}
-
-#endif
-
 @end
