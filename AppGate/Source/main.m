6e526135e93a4052c261e3da440451188155c4e3
Will Ross <paxswill@gmail.com>
2010-11-26 11:00:11 -0500

    Some sort of special hell
    Trying to get the Obj-C runtime linked in was a pain

--- AppGate/Source/main.m
+++ AppGate/Source/main.m
@@ -1,17 +1,19 @@
 //
-//  main.c
+//  main.m
 //  AppGate
 //
 //  Created by Will Ross on 11/26/10.
 //  Copyright 2010 Naval Research Lab. All rights reserved.
 //
 
-#include <CoreFoundation/CoreFoundation.h>
+#import <Foundation/Foundation.h>
 
 int main (int argc, const char * argv[]) {
 
+	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 	// insert code here...
-	CFShow(CFSTR("Hello, World!\n"));
+	NSLog(@"Hello, World!");
+	[pool drain];
     return 0;
 }
 
