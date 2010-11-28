32a1cddbffb4f67ffa105dbde281bd2f96bae847
Will Ross <paxswill@gmail.com>
2010-11-28 07:20:38 -0500

    Added config file support to AppGate

--- AppGate/Source/main.m
+++ AppGate/Source/main.m
@@ -10,18 +10,39 @@
 #import "PXClient.h"
 
 int main (int argc, const char * argv[]) {
-
+	//Make an autorelease pool
 	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-	//test the client
+	
+	//If no path given, set the default one
+	NSString *configPath;
+	if(argc == 2){
+		configPath = [NSString stringWithUTF8String:argv[1]];
+	}else{
+		configPath = @"./config.plist";
+	}
+	//Read in the configuration variables
+	NSDictionary *configuration = [NSDictionary dictionaryWithContentsOfFile:configPath];
+	if(configuration == nil){
+		//The configuration is invalid.
+		NSLog(@"Invalid Cnfiguration file. Either place a proper plist named 'config.plist' in the current directory, or specify one following the command.");
+		exit(1);
+	}
+	NSString *host = [configuration objectForKey:@"storeHost"];
+	int port = [[configuration objectForKey:@"portNumber"] intValue];
+	NSURL *certURL = [NSURL URLWithString:[configuration objectForKey:@"CACertificate"]];
+	NSURL *keyURL = [NSURL URLWithString:[configuration objectForKey:@"keyFile"]];
+	NSString *keyPassword = [configuration objectForKey:@"keyPassword"];
+	
+	//This is just testing the client
 	PXClient *testClient = [[PXClient alloc] init];
-	if(![testClient connectToServer:@"127.0.0.1" onPort:6968]){
+	if(![testClient connectToServer:host onPort:port]){
 		NSLog(@"Connection failed");
 	}else{
 		NSLog(@"Connection succeeded!");
 	}
-	[testClient prepareSSL];
-	[testClient loadCA:[NSURL URLWithString:@"file:///Users/paxswill/Developer/School/CS472/SecureGateway/demoCA/cacert.pem"]];
-	[testClient loadCertificate:[NSURL URLWithString:@"/Users/paxswill/Developer/School/CS472/SecureGateway/AppGate/key.pem"]];
+	[testClient prepareSSLConnection];
+	[testClient loadCA:certURL];
+	[testClient loadKey:keyURL withPassword:keyPassword];
 	[testClient openSSLConnection];
 	
 	[testClient closeConnection];
