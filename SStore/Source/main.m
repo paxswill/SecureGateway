5662c544bf00db0ec6bbf1e025e23c359f88ad57
Will Ross <paxswill@gmail.com>
2010-11-28 02:53:34 -0500

    Change to the new (password enabled) SSL API

--- SStore/Source/main.m
+++ SStore/Source/main.m
@@ -46,7 +46,7 @@ int main (int argc, const char * argv[]) {
 	//Try setting up SSL
 	[testServer prepareSSLConnection];
 	[testServer loadCA:[NSURL URLWithString:@"file:///Users/paxswill/Developer/School/CS472/SecureGateway/demoCA/cacert.pem"]];
-	[testServer loadCertificate:[NSURL URLWithString:@"/Users/paxswill/Developer/School/CS472/SecureGateway/SStore/key.pem"]];
+	[testServer loadKey:[NSURL URLWithString:@"/Users/paxswill/Developer/School/CS472/SecureGateway/SStore/key.pem"] withPassword:@"webbCenter"];
 	[testServer openSSLConnection];
 	
 	//Close the connection
