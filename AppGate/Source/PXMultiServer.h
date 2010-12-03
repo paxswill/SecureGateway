f4d1f56476c0272e39b8ba295324df2dae0cdb69
Will Ross <paxswill@gmail.com>
2010-12-03 10:53:11 -0500

    Multi Server junk

--- AppGate/Source/PXMultiServer.h
+++ AppGate/Source/PXMultiServer.h
@@ -7,11 +7,15 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "PXServer.h"
 
-
-@interface PXMultiServer : NSObject {
+@interface PXMultiServer : PXServer {
 @private
-    
+    NSMutableDictionary *fd2ip, *ip2fd;
 }
+
+@property (readwrite, retain) NSMutableDictionary *fd2ip, *ip2fd;
+
+-(void)openConnection;
 
 @end
--- AppGate/Source/PXMultiServer.m
+++ AppGate/Source/PXMultiServer.m
@@ -8,12 +8,26 @@
 
 #import "PXMultiServer.h"
 
+@interface PXMultiServer()
+@property (readwrite, nonatomic, getter=isConnected) BOOL connected;
+@property (readwrite, nonatomic, getter=isSecure) BOOL secure;
+	@property (readwrite, nonatomic, getter=isListeningForData) BOOL listeningForData;
+-(void)privateListen;
+@end
+
 
 @implementation PXMultiServer
+
+@synthesize fd2ip, ip2fd;
+@synthesize connected;
+@synthesize secure;
+@synthesize listeningForData;
 
 - (id)init {
     if ((self = [super init])) {
         // Initialization code here.
+		fd2ip = [[NSMutableDictionary alloc] init];
+		ip2fd = [[NSMutableDictionary alloc] init];
     }
     
     return self;
@@ -23,6 +37,65 @@
     // Clean-up code here.
     
     [super dealloc];
+}
+
+
+//These two methods are really close, just some optimization for speed and
+// additions for extra connection tracking
+
+-(void)openConnection:(int)socketNum{
+	//At this time, the socket should have an incoming connection
+	struct sockaddr_in *clientAddress;
+	socklen_t clientAddressLength;
+	int newDataSocket = accept(socketNum, (struct sockaddr *)clientAddress, &clientAddressLength);
+	if(newDataSocket < 0){
+		NSLog(@"Connection failed. Closing out. Error: %s", strerror(errno));
+		close(self.listeningSocket);
+	}
+	//Now save the info (file handle, IP address) in the dictionary
+	NSNumber *fd = [NSNumber numberWithInt:newDataSocket];
+	NSString *ip = [NSString stringWithUTF8String:inet_ntoa(clientAddress->sin_addr)];
+	[self.ip2fd setObject:fd forKey:ip];
+	[self.fd2ip setObject:ip forKey:fd];
+}
+
+-(void)privateListen{
+	//Make an autorelease pool
+	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+	
+	while(![listenThread isCancelled] && self.connected){
+		//Is there stuff to read?
+		fd_set listenSet;
+		FD_SET(self.mainSocket, &listenSet);
+		struct timeval zeroTime;
+		zeroTime.tv_sec = 0;
+		zeroTime.tv_usec = 50;
+		int numReadySockets = select(self.mainSocket + 1, &listenSet, NULL, NULL, &zeroTime);
+		BOOL isSocketReady = FD_ISSET(self.mainSocket, &listenSet) != 0? YES : NO;
+		if(numReadySockets > 0 && isSocketReady && self.delegate != nil){
+			//Prepare the buffers (5MB buffer)
+			size_t bufferSize = 5*1024*1024;
+			void *buffer = malloc(bufferSize);
+			ssize_t numBytesRead;
+			//Use SSL_read for secure connections
+			if(self.secure){
+				numBytesRead = SSL_read(self.sslConnection, buffer, bufferSize);
+			}else{
+				numBytesRead = read(self.mainSocket, buffer, bufferSize);
+			}
+			//Is the connection broken?
+			if(numBytesRead == 0){
+				NSLog(@"Connection Broken");
+				self.connected = NO; 
+				[NSThread exit];
+			}
+			//Give the data to the delegate
+			[delegate recievedData:[NSData dataWithBytes:buffer length:numBytesRead] fromConnection:self];
+			free(buffer);
+		}
+	}
+	//Drain the pool
+	[pool drain];
 }
 
 @end
