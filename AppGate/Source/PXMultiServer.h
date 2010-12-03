07d006aea508eaf5cd436641c1767754cfe7dc3e
Will Ross <paxswill@gmail.com>
2010-12-03 09:31:12 -0500

    Adding a multiple connection server
    It can handle multiple clients at once

---
+++ AppGate/Source/PXMultiServer.h
@@ -0,0 +1,17 @@
+//
+//  PXMultiServer.h
+//  AppGate
+//
+//  Created by Will Ross on 12/3/10.
+//  Copyright 2010 Will Ross. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface PXMultiServer : NSObject {
+@private
+    
+}
+
+@end
---
+++ AppGate/Source/PXMultiServer.m
@@ -0,0 +1,28 @@
+//
+//  PXMultiServer.m
+//  AppGate
+//
+//  Created by Will Ross on 12/3/10.
+//  Copyright 2010 Will Ross. All rights reserved.
+//
+
+#import "PXMultiServer.h"
+
+
+@implementation PXMultiServer
+
+- (id)init {
+    if ((self = [super init])) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)dealloc {
+    // Clean-up code here.
+    
+    [super dealloc];
+}
+
+@end
